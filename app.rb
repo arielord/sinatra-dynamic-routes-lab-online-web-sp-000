a2d81227f8ee03cc0a04fd7ebd2277d062a3cf9b
Ariel <arielord@buffalo.edu>
2019-03-28 01:19:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,6 +28,8 @@ class App < Sinatra::Base
     phrase.strip + "."
   end
   get '/operation/:number1/:number2' do
-    
+    num1 = params[:number1]
+    num2 = params[:number2]
+    op = params[:operation]
   end
 end
